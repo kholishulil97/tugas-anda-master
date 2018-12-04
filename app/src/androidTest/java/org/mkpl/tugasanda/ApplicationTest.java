1e5b1d1cd490c8bc1f79df003ce1323caa2471eb
Kholish <kholishulil.ku@gmail.com>
2018-12-04 20:02:08 +0700

    Initial commit
    versi 1.0.0

---
+++ app/src/androidTest/java/org/mkpl/tugasanda/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.mkpl.tugasanda;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ *
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
